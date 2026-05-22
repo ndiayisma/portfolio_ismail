b4289df911b0b8743e8886471a253e9a603bcaaa
ndiayisma <ismail.ndiaye@lycee-faure.fr>
2026-05-22 17:02:10 +0200

    Add Links for projects

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -4,22 +4,30 @@ const projects = [
     {
         title: "Projet Conventio",
         description: "Une application web de gestion de conventions de stage pour les étudiants de BTS.",
-        technologies: ["Symfony", "PHP", "MySQL"]
+        technologies: ["Symfony", "PHP", "MySQL"],
+        image: "/ExempleConventio.png",
+        link: "https://github.com/maxervj/Conventio"
     },
     {
         title: "GSB-Express",
         description: "Une API REST pour la gestion de données avec intégration GitHub et Codespace.",
-        technologies: ["ExpressJS", "Postman API", "GitHub", "MongoDB", "Android Studio"]
+        technologies: ["ExpressJS", "Postman API", "GitHub", "MongoDB", "Android Studio"],
+        image: "/GSB.png",
+        link: "https://github.com/ndiayisma/api-rest-gsb"
     },
     {
         title: "Cylis",
         description: "Un site vitrine réalisé avec le Groupe SmileRepair pour la promotion de leurs services basé en Algérie.",
-        technologies: ["WordPress"]
+        technologies: ["WordPress"],
+        image: "/ExempleCylis.png",
+        link: "https://cylis.net"
     },
     {
         title: "Projet Personnel",
-        description: "Un projet personnel de développement web pour expérimenter de nouvelles technologies.",
-        technologies: ["Next.JS", "TailwindCSS"]
+        description: "Un projet personnel de développement web servant de guide et glossaires pour les débutants dans la communauté FGC (Tekken, Street Fighter )(Fighting Game Community).",
+        technologies: ["Next.JS", "TailwindCSS"],
+        image: "/TeaserProjetPerso.jpeg",
+        link: "https://github.com/ndiayisma/ism-fgc-glossary"
     }
 ];
 
@@ -34,19 +42,24 @@ export const Projects = () => {
                 </p>
                 <div className="grid md:grid-cols-2 gap-8 mt-12">
                     {projects.map((project, index) => (
-                        <div key={index} className="bg-gray-900 border border-gray-800 p-6 rounded-lg hover:translate-y-1 hover:border-green-500 transition-colors transition-transform">
-                            <h3 className="font-bold text-lg text-white mb-4">{project.title}</h3>
-                            <p className="text-gray-300">
-                                {project.description}
-                            </p>
-                            <div>
-                                {project.technologies.map((tech, idx) => (
-                                    <span key={idx} className="inline-block bg-green-500 text-white text-xs px-2 py-1 rounded-full mr-2 mt-4">
-                                        {tech}
-                                    </span>
-                                ))}
+                        <a key={index} href={project.link} target="_blank" rel="noopener noreferrer" className="block">
+                            <div className="bg-gray-900 border border-gray-800 rounded-lg hover:translate-y-1 hover:border-green-500 transition-colors transition-transform overflow-hidden cursor-pointer h-full">
+                                <img src={project.image} alt={project.title} className="w-full h-48 object-cover" />
+                                <div className="p-6">
+                                    <h3 className="font-bold text-lg text-white mb-4">{project.title}</h3>
+                                    <p className="text-gray-300">
+                                        {project.description}
+                                    </p>
+                                    <div>
+                                        {project.technologies.map((tech, idx) => (
+                                            <span key={idx} className="inline-block bg-green-500 text-white text-xs px-2 py-1 rounded-full mr-2 mt-4">
+                                                {tech}
+                                            </span>
+                                        ))}
+                                    </div>
+                                </div>
                             </div>
-                        </div>
+                        </a>
                     ))}
                 </div>
             </div>
